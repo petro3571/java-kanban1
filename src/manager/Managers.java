cf05bed790fd1955bf6a7c0cb8459921029a3d16
PETRO <dim4petro357@yandex.ru>
2025-02-10 15:13:51 +0800

    Чексайтл +++++

--- src/manager/Managers.java
+++ src/manager/Managers.java
@@ -7,6 +7,7 @@ public final class Managers {
     public static TaskManager getDefaultTaskManager() {
         return new InMemoryTaskManager();
     }
+    
     public static HistoryManager getDefaultHistoryManager() {
         return new InMemoryHistoryManager();
     }
